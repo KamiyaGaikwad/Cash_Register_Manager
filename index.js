bb590d64732e36d489e99e5c430559c364588985
Kamiya <kamitarg88@gmail.com>
2021-08-28 00:18:49 +0530

    Showing content on button click

--- index.js
+++ index.js
@@ -3,8 +3,16 @@ const cashGiven = document.querySelector("#cash-given");
 const checkButton = document.querySelector("#check-button");
 const message = document.querySelector("#error-message");
 const noOfNotes = document.querySelectorAll(".no-of-notes");
+const nextButton = document.querySelector("#next-button");
+const currencyTable = document.querySelector("#currency-table");
+const cashGivenLabel = document.querySelector("#cash-given-label");
 
 const availableNotes = [2000, 500, 100, 20, 10, 5, 1];
+hideCashGiven();
+hideCurrencyTable();
+nextButton.addEventListener("click", function showCashGivenAndCheckButton(){
+    showCashGiven();
+})
 checkButton.addEventListener("click", function validateBillAndCashAmount(){
     hideMessage();
     console.log(isNaN(billAmount.value))
@@ -17,6 +25,7 @@ checkButton.addEventListener("click", function validateBillAndCashAmount(){
             if (cashGiven.value >= billAmount.value) {
                 const amountToBeReturned = cashGiven.value - billAmount.value;
                 calculateChange(amountToBeReturned);
+                showCurrencyTable();
             }else {
                showMessage("The cash provided should atleast be equal to the bill amount"); 
             }
@@ -48,3 +57,23 @@ function hideMessage(){
 
 //bonus bill amount as string show error 
 // invalid bill amount, enter in number 
+
+function hideCashGiven(){
+    cashGivenLabel.style.display = "none";
+    cashGiven.style.display = "none";
+    checkButton.style.display = "none";
+}
+
+function hideCurrencyTable() {
+    currencyTable.style.display = "none";
+}
+
+function showCashGiven(){
+    cashGivenLabel.style.display = "block";
+    cashGiven.style.display = "block";
+    checkButton.style.display = "block";
+}
+
+function showCurrencyTable() {
+    currencyTable.style.display = "block";
+}
